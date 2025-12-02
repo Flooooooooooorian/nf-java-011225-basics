4ff23f82fc0dbf662a9c7cb93bdef4ef298a6093
Flooooooooooorian <florian.weber@neuefische.de>
2025-12-02 15:15:28 +0100

    Add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String value = "Florian";
+        if (value == "Florian") {
+
+        } else if (value == "Dominic") {
+
+        } else if (value == "Kevin") {
+
+        } else if (value == "Thomas") {
+
+        }
+
+        switch (value) {
+            case "Florian": {
+                System.out.println("Hallo Florian");
+                break;
+            }
+            case "Dominic": {
+                System.out.println("Hallo Dominic");
+            }
+        }
+
+
+        String result = value.length() > 10 ? "Der Wert ist länger als 10 Zeichen" : "Der Wert ist nicht länger als 10 Zeichen";
+        System.out.println(result);
+
+        String myValue = "     ";
+        System.out.println(myValue.isBlank());
+        System.out.println(myValue.isEmpty());
+
+    }
+}
